c98c1766f746d007bd449ddaecc005dcfb5f5984
Ms-Shahid <ms2406shahid@gmail.com>
2021-08-20 23:20:26 +0530

    Compare two lines

--- LineCalculations.java
+++ LineCalculations.java
@@ -14,10 +14,12 @@ public class LineCalculations {
 			
 		Integer length1 = (int) Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
 		Integer length2 = (int) Math.sqrt(Math.pow((a2-a1),2) + Math.pow((b2-b1),2));
-		if (length1.equals(length2)) {
-			System.out.println("lines are equal");
-		}else {
-			System.out.println("lines are Not equal");
+		
+		int compare = length1.compareTo(length2);
+		switch (compare) {
+		case 0: System.out.println("lines are equal");break;
+		case 1: System.out.println("line1 is less than line2");break;
+		default: System.out.println("line1 is grether than line2");
 		}
 	}
 }
