e573e9e15975060255bf0709e74734a7c194222e
Ms-Shahid <ms2406shahid@gmail.com>
2021-08-20 23:15:25 +0530

    Check Equality of lines

--- LineCalculations.java
+++ LineCalculations.java
@@ -12,9 +12,12 @@ public class LineCalculations {
 		int a1 = 5, a2 = 15;
 		int b1 = 4, b2 = 8;
 			
-		double length1 = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
-		double length2 = Math.sqrt(Math.pow((a2-a1),2) + Math.pow((b2-b1),2));
-		System.out.println("Length of Line joining points " +"("+x1 + "," + y1 +")" +" " + "("+x2 + "," +y2 +")" + " is " + length1);
-		System.out.println("Length of Line joining points " +"("+a1 + "," + b1 +")" +" " + "("+a2 + "," +b2 +")" + " is " + length2);
+		Integer length1 = (int) Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
+		Integer length2 = (int) Math.sqrt(Math.pow((a2-a1),2) + Math.pow((b2-b1),2));
+		if (length1.equals(length2)) {
+			System.out.println("lines are equal");
+		}else {
+			System.out.println("lines are Not equal");
 		}
+	}
 }
